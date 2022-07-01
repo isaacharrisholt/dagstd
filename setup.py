1abb1f8baaf85b86cab9a58cf0af6f56d82c0707
Isaac Harris-Holt <isaac@myurbanjungle.com>
2022-07-01 14:32:57 +0100

    Bump hotfix version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'Dagstd'
 with open('README.rst', 'r', encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
